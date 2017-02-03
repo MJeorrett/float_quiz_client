c4f42a2ecff958b9e3bd1b3713a6c599fef347b0
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-03 08:46:56 +0000

    displaying all environement variables

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const env_vars = JSON.stringify( process.env )
     return (
       <div>
         <h1>Welcome to the Float Cash Flow Quiz</h1>
-        <p>API_KEY: {process.env.API_KEY}</p>
+        <p>environment variables: {env_vars}</p>
       </div>
     )
   }
