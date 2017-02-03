7c7bcc7cb0cad2db0dda4ad739e2d14524850a16
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-03 09:57:26 +0000

    adding name input to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const Welcome = () => {
-  return <h3>Welcome to the Float Cash Flow Quiz</h3>
+  let nameInput
+  return (
+    <div>
+      <h3>Welcome to the Float Cash Flow Quiz</h3>
+      <form>
+        <input
+          ref={ node => nameInput = node }
+          type="text"
+          placeholder="Please enter your name" />
+        <button onClick={ (ev) => {
+            ev.preventDefault()
+            console.log("name entered:", nameInput.value)
+          }
+        }>Next</button>
+      </form>
+    </div>
+  )
 }
 
 export default Welcome
