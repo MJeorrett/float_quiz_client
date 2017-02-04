17cc8395461539324ba9b821f6605ece6ff0c90b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-04 18:12:01 +0000

    adding total_score and selected_answer_score to game_state_reducer

--- src/redux/reducers/game_state_reducer.js
+++ src/redux/reducers/game_state_reducer.js
@@ -2,7 +2,9 @@ import * as types from '../actions/types'
 
 const defaultState = {
   player_name: "",
-  current_question_index: 0
+  current_question_index: 0,
+  total_score: 0,
+  selected_answer_score: null
 }
 
 const game_state_reducer = ( state = defaultState, action ) => {
@@ -13,7 +15,9 @@ const game_state_reducer = ( state = defaultState, action ) => {
       })
     case types.NEXT_QUESTION:
       return Object.assign( {}, state, {
-        current_question_index: state.current_question_index + 1
+        current_question_index: state.current_question_index + 1,
+        total_score: state.total_score + state.selected_answer_score,
+        selected_answer_score: null
       })
     default:
       return state
