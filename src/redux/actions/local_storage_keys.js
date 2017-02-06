3ab4349167b5bdbf27d3af622aba9cdcc2d8b855
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-02-06 15:00:23 +0000

    minor refactoring standardising local storage keys

--- src/redux/actions/local_storage_keys.js
+++ src/redux/actions/local_storage_keys.js
@@ -1,3 +1,3 @@
 export const PLAYER_NAME = 'playerName'
 export const CURRENT_QUESTION_INDEX = 'currentQuestionIndex'
-export const TOTAL_SCORE = 'totalScoreKey'
+export const TOTAL_SCORE = 'totalScore'
